27f5c7abedbe3a9056b462e9b9db547953825d3b
Victor Häggqvist <victor@snilius.com>
2014-06-27 19:26:21 +0200

    index update

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
   </head>
 
   <body>
+  <a href="https://github.com/victorhaggqvist/touch-imagelightbox"><img id="fork-ribbon" style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 <?php
 require_once 'images.php';
 
@@ -32,6 +33,7 @@ $headings = array(
     <div id="container">
       <header>
         <h1>Image Lightbox<span>Responsive &amp; Touch-Friendly</span></h1>
+        <iframe id="fork-button" src="http://ghbtns.com/github-btn.html?user=victorhaggqvist&repo=touch-imagelightbox&type=fork"  allowtransparency="true" frameborder="0" scrolling="0" width="53" height="20"></iframe>
       </header>
 
       <?php
@@ -50,6 +52,7 @@ $headings = array(
        ?>
 
       <footer>
+        <p>View source of demos on <a href="https://github.com/victorhaggqvist/touch-imagelightbox/tree/gh-pages">Github</a>.</p>
         <p>All photos are licensed <a href="https://creativecommons.org/licenses/by/2.0/">CC BY 2.0</a>. For full credits please referer to <a href="./images.md">images.md</a></p>
       </footer>
     </div>
