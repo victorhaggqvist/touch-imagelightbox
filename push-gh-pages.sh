12d183c54893ca96c3b432c5fbce7e71bfd1bb5e
Victor Häggqvist <victor@snilius.com>
2016-01-17 00:05:26 +0100

    fix gh-pages script

--- push-gh-pages.sh
+++ push-gh-pages.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-git clone --depth=0 git@github.com:victorhaggqvist/touch-imagelightbox.git --branch gh-pages gh-pages
+rm -rf gh-pages
+git clone --depth=1 git@github.com:victorhaggqvist/touch-imagelightbox.git --branch gh-pages gh-pages
 rm -rf gh-pages/*
 cp demo/* gh-pages
+pushd gh-pages
 git add .
 git commit -m "$(git rev-parse --git-dir=../.git --short HEAD)"
-git push origin gh-pages
+git push origin gh-pages --force
+popd
